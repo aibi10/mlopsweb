c690b14dcc466a91163888d2bf59a15a487c5413
aibi10 <isingh.abhishek10@gmail.com>
2024-09-17 07:47:23 +0400

    folder structure updation

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py"
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception",
+    "test/unit/__init__.py",
+    "test/integration/__init__.py",
+    "__init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiment.ipynb"
+]
+
+for file_path in list_of_files:
+        # Convert string to Path object
+        path = Path(file_path)
+        
+        # Create parent directories if they don't exist
+        path.parent.mkdir(parents=True, exist_ok=True)
+        
+        # Create the file
+        path.touch(exist_ok=True)
+        print(f"Created: {path}")
